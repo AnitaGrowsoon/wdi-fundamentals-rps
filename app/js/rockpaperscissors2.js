27e3ca27691cf40596fc7da41c754c0d9dd6e642
Anita Growsoon <anitagrowsoon@gmail.com>
2015-09-23 14:22:44 -0400

    REALLY fixed getWinner, played around with error messages, added playToX function

--- app/js/rockpaperscissors2.js
+++ app/js/rockpaperscissors2.js
@@ -74,34 +74,97 @@ function getWinner(playerMove,computerMove) {
 }
 
 function playToFive() {
-    console.log("Let's play Rock, Paper, Scissors");
+    console.log("Let's play Rock, Dynamite with a cuttable wick, Scissors");
     var playerWins = 0;
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    getPlayerMove();
-    getComputerMove();
-    getWinner();
-    while(playerWins <= 5 && computerWins <= 5) {
+    while(playerWins < 5 && computerWins < 5) {
+        winner = getWinner();
         if (winner === 'player') {
             playerWins += 1;
-            console.log("You win this time, punk.");
-            console.log(playerMove + " beats " + computerMove + ".");
-            console.log("Now the score is " playerWins + ":" + computerWins + ".");
-            console.log("If you're feeling lucky, let's play again.");
+            console.log(playerMove + " beats " + computerMove + "." + '\n' + "Now the score is :" + playerWins + " : " + computerWins + "." + '\n');    
         } else if (winner === 'computer') {
             computerWins += 1;
-            console.log(computerMove + " beats " + playerMove + "! Muahahahaha");
-            console.log("Now the score is " playerWins + ":" + computerWins + ".");
-            console.log("Still feeling lucky? Let's play again.");
+            console.log(playerMove + " beats " + computerMove + "." + '\n' + "Now the score is :" + playerWins + " : " + computerWins + "." + '\n');    
         } else if (winner === 'tie') {
-            console.log("If you're not first, you're last. Rematch?";
+            console.log("If you're not first, you're last. Rematch?");
         } else {
             console.log("404 - error - your brain not found");
         }
     console.log("There's only room for one of us in this town.");
     return [playerWins, computerWins];
 }
+}
 
 
 // If you are interested in an additional challenge, try writing another function called playTo(x) that allows us to play Rock Paper Scissors until either the player or the computer has won x times. Feel free to steal liberally from playToFive().
+//added dialogs. lol
 
+function playToX(x) {
+    console.log("Let's play Rock, Paper, Scissors");
+    var playerWins = 0;
+    var computerWins = 0;
+    var winner;
+    var x;
+    while(playerWins < (x - 1) && computerWins < (x - 1) ){
+        winner = getWinner();
+        if (winner === 'player') {
+            playerWins += 1;
+            console.log('...');
+            console.log("You win this time, punk. ");
+            console.log(playerMove + " beats " + computerMove + ".");
+            console.log("Now the score is: " + playerWins + ", " + computerWins + ".");
+            console.log("If you're feeling lucky, let's play again.");
+            console.log('');
+        } else if (winner === 'computer') {
+            computerWins += 1;
+            console.log('...');
+            console.log(computerMove + " beats " + playerMove + "! Muahahahaha");
+            console.log("Now the score is: " + playerWins + ", " + computerWins + ".");
+            console.log("Still feeling lucky? Let's play again.");
+            console.log('');
+        } else if (winner === 'tie') {
+            console.log('...');
+            console.log("If you're not first, you're last. Rematch?" + '\n');
+        } else {
+            console.log("Did you enter 'paper'? Tsk. Tsk." + '\n' + "Make sure you enter a correct response." + '\n')
+        }
+}   while(playerWins < x && computerWins < x) {
+        winner = getWinner();
+            if ((playerWins == (x - 1)) && winner === 'player') {
+                playerWins += 1;
+                console.log('...');
+                console.log("You beat me! I demand a recount!");
+                console.log('');
+            } else if ((computerWins = (x - 1)) && winner === 'computer') {
+                computerWins += 1;
+                console.log('...');
+                console.log("You lost. Wanna cry about it?");
+                console.log('');
+            } else if ((playerWins == (x - 1)) || (winner === 'player')) {
+                playerWins += 1;
+                console.log('...');
+                console.log("You win this time, punk. ");
+                console.log(playerMove + " beats " + computerMove + ".");
+                console.log("Now the score is: " + playerWins + ", " + computerWins + ".");
+                console.log("If you're feeling lucky, let's play again.");
+                console.log('');
+            } else if ((computerWins = (x - 1)) || winner === 'computer') {
+                computerWins += 1;
+                console.log('...');
+                console.log(computerMove + " beats " + playerMove + "! Muahahahaha");
+                console.log("Now the score is: " + playerWins + ", " + computerWins + ".");
+                console.log("Still feeling lucky? Let's play again.");
+                console.log('');  
+            } else if (winner === 'tie') {
+                console.log('...');
+                console.log("If you're not first, you're last. Rematch?" + '\n');
+            } else {
+                console.log("Did you enter 'paper'? Tsk. Tsk." + '\n' + "Make sure you enter a correct response." + '\n')
+            }
+        if ((playerWins == x) || (computerWins == x)) {
+        console.log('\n' + "There's only room for one of us in this town.");
+        return [playerWins, computerWins];
+        }
+    }
+}
