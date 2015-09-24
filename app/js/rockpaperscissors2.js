9583c11439af2eac0a57491e7663c1d9d65b2f15
Anita Growsoon <anitagrowsoon@gmail.com>
2015-09-24 15:42:28 -0400

    took out instructions for simplified code, saved as rockpaperscissors2

--- app/js/rockpaperscissors2.js
+++ app/js/rockpaperscissors2.js
@@ -1,6 +1,5 @@
-
 function getInput() {
-    console.log("Please choose either 'rock', 'dynamite', or 'scissors'.")
+    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
     return prompt();
 }
 function randomPlay() {
@@ -8,11 +7,12 @@ function randomPlay() {
     if (randomNumber < 0.33) {
         return "rock";
     } else if (randomNumber < 0.66) {
-        return "dynamite";
+        return "paper";
     } else {
         return "scissors";
     }
 }
+
 
 function getPlayerMove(move) {
     return playerMove = move || getInput();
@@ -23,141 +23,75 @@ function getComputerMove(move) {
 }
 
 function getWinner(playerMove,computerMove) {
+    var winner;
     var playerMove = getPlayerMove();
     var computerMove = getComputerMove();
-    var winner;
     if (playerMove === 'quit') {
         winner = 'quit';
-        return "quit";
     } else if (playerMove == computerMove) {
         winner = 'tie';
+    } else if (((playerMove == 'rock') && (computerMove == 'paper')) || 
+              ((playerMove == 'paper') && (computerMove == 'scissors')) || 
+              ((playerMove == 'scissors') && (computerMove == 'rock'))) 
+              {
+        winner = 'computer';
+    } else if (((playerMove == 'rock') && (computerMove == 'scissors')) || 
+              ((playerMove == 'paper') && (computerMove == 'rock')) || 
+              ((playerMove == 'scissors') && (computerMove == 'paper'))) 
+              {
+        winner = 'player';
     } else { 
-        switch (playerMove) {
-            case 'rock':
-                if (computerMove === 'dynamite') {
-                    winner = 'computer';
-                } else if (computerMove === 'scissors') {
-                    winner = 'player';
-                } break;
-            case 'dynamite':
-                if (computerMove === 'scissors') {
-                    winner = 'computer';
-                } else if (computerMove === 'rock') {
-                    winner = 'player';
-                } break;
-            case 'scissors':
-                if (computerMove === 'rock') {
-                    winner = 'computer';
-                } else if (computerMove === 'dynamite') {
-                    winner = 'player';
-                } break;
-            default : return 'you entered an incorrect answer. try again';
+        winner = null;
         }
-        
-    }
-    console.log("player chose" + ": " + playerMove);
-    console.log("computer chose" + ": " + computerMove);
+        console.log("player chose" + ": " + playerMove);
+        console.log("computer chose" + ": " + computerMove);
     return winner;
 }
 
 function playToFive() {
-    console.log("Let's play Rock, Dynamite with a cuttable wick, Scissors");
+    console.log("Let's play Rock, Paper, Scissors");
+    console.log("To quit, type 'quit'");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    while(playerWins < 5 && computerWins < 5) {
+    while(playerWins < 5 && computerWins < 5 ) {
         winner = getWinner();
         if (winner === 'quit') {
-            return "Fine. Quitter."
+            return "Fine. Quitter.";
         } else if (winner === 'player') {
             playerWins += 1;
             console.log(playerMove + " beats " + computerMove + "." + '\n' + "Now the score is :" + playerWins + " : " + computerWins + "." + '\n');    
-            console.log("To quit, type 'quit'." + '\n');
         } else if (winner === 'computer') {
             computerWins += 1;
             console.log(playerMove + " beats " + computerMove + "." + '\n' + "Now the score is :" + playerWins + " : " + computerWins + "." + '\n');
-            console.log("To quit, type 'quit'." + '\n');
         } else if (winner === 'tie') {
-            console.log("If you're not first, you're last. Rematch?");
-            console.log("To quit, type 'quit'." + '\n');
+            console.log("If you're not first, you're last. -_- No one wins.");
         } else {
-            console.log("Did you enter 'paper'? Tsk. Tsk.");
-            console.log("Make sure you enter 'rock', 'dynamite', or 'scissors'.")
-            console.log("To quit, type 'quit'." + '\n');
+            console.log("you did not enter a valid response' + '\n' + 'to quit, type 'quit'");
         }
-}   console.log("There's only room for one of us in this town.");
+}   console.log("GAME OVER");
     return [playerWins, computerWins];
 }
 
-
-
-function playToX(x) {
+function playTo(x) {
     console.log("Let's play Rock, Paper, Scissors");
+    console.log("To quit, type 'quit'");
     var playerWins = 0;
     var computerWins = 0;
-    var winner;
-    var x;
-    while(playerWins < (x - 1) && computerWins < (x - 1) ){
+    while(playerWins < x && computerWins < x ) {
         winner = getWinner();
         if (winner === 'quit') {
-            return "Fine. Quitter."
+            return "Fine. Quitter.";
         } else if (winner === 'player') {
             playerWins += 1;
-            console.log('...');
-            console.log("You win this time, punk. ");
-            console.log(playerMove + " beats " + computerMove + ".");
-            console.log("Now the score is: " + playerWins + ", " + computerWins + ".");
-            console.log("If you're feeling lucky, let's play again.");
-            console.log('');
+            console.log(playerMove + " beats " + computerMove + "." + '\n' + "Now the score is :" + playerWins + " : " + computerWins + "." + '\n');    
         } else if (winner === 'computer') {
             computerWins += 1;
-            console.log('...');
-            console.log(computerMove + " beats " + playerMove + "! Muahahahaha");
-            console.log("Now the score is: " + playerWins + ", " + computerWins + ".");
-            console.log("Still feeling lucky? Let's play again.");
-            console.log('');
+            console.log(playerMove + " beats " + computerMove + "." + '\n' + "Now the score is :" + playerWins + " : " + computerWins + "." + '\n');
         } else if (winner === 'tie') {
-            console.log('...');
-            console.log("If you're not first, you're last. Rematch?" + '\n');
+            console.log("If you're not first, you're last. -_- No one wins.");
         } else {
-            console.log("Did you enter 'paper'? Tsk. Tsk." + '\n' + "Make sure you enter a correct response." + '\n')
+            console.log("you did not enter a valid response' + '\n' + 'to quit, type 'quit'");
         }
-}   while(playerWins < x && computerWins < x) {
-        winner = getWinner();
-            if ((playerWins == (x - 1)) && winner === 'player') {
-                playerWins += 1;
-                console.log('...');
-                console.log("You beat me! I demand a recount!");
-                console.log('');
-            } else if ((computerWins = (x - 1)) && winner === 'computer') {
-                computerWins += 1;
-                console.log('...');
-                console.log("You lost. Wanna cry about it?");
-                console.log('');
-            } else if ((playerWins == (x - 1)) || (winner === 'player')) {
-                playerWins += 1;
-                console.log('...');
-                console.log("You win this time, punk. ");
-                console.log(playerMove + " beats " + computerMove + ".");
-                console.log("Now the score is: " + playerWins + ", " + computerWins + ".");
-                console.log("If you're feeling lucky, let's play again.");
-                console.log('');
-            } else if ((computerWins = (x - 1)) || winner === 'computer') {
-                computerWins += 1;
-                console.log('...');
-                console.log(computerMove + " beats " + playerMove + "! Muahahahaha");
-                console.log("Now the score is: " + playerWins + ", " + computerWins + ".");
-                console.log("Still feeling lucky? Let's play again.");
-                console.log('');  
-            } else if (winner === 'tie') {
-                console.log('...');
-                console.log("If you're not first, you're last. Rematch?" + '\n');
-            } else {
-                console.log("Did you enter 'paper'? Tsk. Tsk." + '\n' + "Make sure you enter a correct response." + '\n')
-            }
-        if ((playerWins == x) || (computerWins == x)) {
-        console.log('\n' + "There's only room for one of us in this town.");
-        return [playerWins, computerWins];
-        }
-    }
+}   console.log("GAME OVER");
+    return [playerWins, computerWins];
 }
